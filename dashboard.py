01ba77f01dc58d7e224898d86031e6f1b8fa412c
Akshay Suresh <akshay721@gmail.com>
2023-04-13 22:07:10 -0400

    Added server

--- dashboard.py
+++ dashboard.py
@@ -15,6 +15,7 @@ df['dist_state'] = df['Dist Name']+', '+df['State Name']
 ############################################################
 # Creating application
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # Quantity to plot
 quantity_options = [{'label':'Harvested area', 'value':'Area (1000 ha)'},
