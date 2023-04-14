00769e5d9ac5433de8c0eaa66681414d51d2cd8d
Akshay Suresh <akshay721@gmail.com>
2023-04-14 01:06:42 -0400

    Added host

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def display_choropleth(quantity, crop, year):
     return fig
 
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8080)
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
 
 # END OF CODE
 ############################################################
